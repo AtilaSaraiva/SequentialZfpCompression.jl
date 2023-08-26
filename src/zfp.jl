40ad64ceea2378edb812a6112717252e717e74d9
Átila Saraiva <atilasaraiva@gmail.com>
2023-08-26 17:39:17 -0600

    type: making positions atribute to be divided in tail and head positions for speed

--- src/zfp.jl
+++ src/zfp.jl
@@ -1,15 +1,17 @@
 struct CompressedTimeArray{T,N}
     data::Vector{UInt8}
-    positions::Vector{Int64}
+    headpositions::Vector{Int64}
+    tailpositions::Vector{Int64}
     dims::NTuple{N,Int32}
     eltype::Type{T}
 
     function CompressedTimeArray(dtype::DataType, spacedim::Integer...; timedim::Integer)
         data = Vector{UInt8}()
-        positions = Vector{Int64}()
+        headpositions = Vector{Int64}()
+        tailpositions = Vector{Int64}()
         eltype = dtype
-        dims = (spacedim..., timedim)
-        return new{dtype, length(dims)}(data, positions, dims, eltype)
+        dims = (timedim, spacedim...)
+        return new{dtype, length(dims)}(data, headpositions, tailpositions, dims, eltype)
     end
 end
 
@@ -19,7 +21,8 @@ function CompressedTimeArray(array::AbstractArray{<:AbstractFloat}; timedim::Int
     data = zfp_compress(array, write_header=false)
     fileSize = length(data)
     append!(compArray.data, data)
-    push!(compArray.positions, fileSize)
+    push!(compArray.tailpositions, 1)
+    push!(compArray.headpositions, fileSize)
 
     return compArray
 end
