229259af760906c017718d5c6245668c16dc9e7d
Átila Saraiva <atilasaraiva@gmail.com>
2023-09-01 00:43:47 -0600

    tests: multifile compression

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using SequentialCompression
 using Test
 
-@testset "SequentialCompression.jl" begin
+@testset "in memory compression" begin
     for dtype in [ Float32, Float64 ]
         for (n, m) in [[10, 10], [100, 100], [1000, 1000]]
             A = [ dtype(i + j) for i=1:n, j=1:m ]
@@ -24,6 +24,27 @@ using Test
             end
 
             @test Bc[:] == B
+        end
+    end
+end
+
+@testset "multifile compression" begin
+    for dtype in [ Float32, Float64 ]
+        for (n, m) in [[100, 100], [1000, 1000]]
+            B = rand(dtype, n,m,3)
+
+            Bc = CompressedMultiFileArraySeq(dtype, n,m)
+
+            for i = 1:3
+                append!(Bc, B[:,:,i])
+            end
+
+            @test size(Bc) == (n, m, 3)
+            @test ndims(Bc) == 3
+
+            @test Bc[1] == B[:,:,1]
+            @test Bc[2] == B[:,:,2]
+            @test Bc[3] == B[:,:,3]
         end
     end
 end
