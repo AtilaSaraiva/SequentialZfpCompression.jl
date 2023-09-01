8f841d2737e6e479131220a23fcc148d00280294
Átila Saraiva <atilasaraiva@gmail.com>
2023-09-01 12:26:17 -0600

    env: added Serizalization

--- src/SequentialCompression.jl
+++ src/SequentialCompression.jl
@@ -3,13 +3,15 @@ module SequentialCompression
 using ZfpCompression: zfp_compress, zfp_decompress!
 using TiledIteration: SplitAxes
 using Base.Threads: @threads, nthreads
+using Serialization: serialize, deserialize
 
 abstract type AbstractCompArraySeq end
 
 include("seqcomp.jl")
 include("multifile.jl")
 include("unified_constructor.jl")
+include("filedump.jl")
 
-export SeqCompressor
+export SeqCompressor, save, load
 
 end
